16d6b478f2015627cdb50d6f26a2c47039ebe818
Fyodor Zaletov <zaletovfyodor@gmail.com>
2024-09-19 14:48:40 +0300

    add base modules, core code

---
+++ Assets/Scripts/Core/FSM/Fsm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Core.FSM
+{
+    public class Fsm : IFsm
+    {
+        private FsmState _currentState;
+        protected Dictionary<Type, FsmState> _states = new Dictionary<Type, FsmState>();
+
+        public virtual void Init()
+        {
+        }
+
+        public void SetState<T>() where T : FsmState
+        {
+            var type = typeof(T);
+
+            if (_states.TryGetValue(type, out var newState))
+            {
+                SetState(newState);
+            }
+            else
+            {
+                Debug.LogError($"{this} : FSM not found state - {type}");
+            }
+        }
+
+        public void SetState(FsmState state)
+        {
+            if (_currentState == state)
+                return;
+
+            _currentState?.Exit();
+            _currentState = state;
+            _currentState.Enter();
+        }
+
+        public void SetStartState()
+        {
+            SetState(_states.First().Value);
+        }
+
+        public void Reset()
+        {
+            _currentState?.Exit();
+            _currentState = null;
+        }
+
+        public void Update()
+        {
+            _currentState?.Update();
+        }
+    }
+
+    public interface IFsm
+    {
+        public void Init()
+        {
+        }
+
+        public void SetState<T>() where T : FsmState
+        {
+        }
+
+        public void SetStartState();
+
+        public void Reset();
+    }
+}
---
+++ Assets/Scripts/Core/FSM/FsmManager.cs
@@ -0,0 +1,72 @@
+﻿using Base;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Core.FSM
+{
+    public class FsmManager : Singleton<FsmManager>
+    {
+        [Inject] private DiContainer _diContainer;
+        
+        private Dictionary<Type, Fsm> _currentFsms = new ();
+        private Fsm _activeFsm;
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            foreach (var currentFsm in _currentFsms)
+                currentFsm.Value.Init();
+        }
+
+        public void SetActiveFsm<T>() where T : Fsm, new()
+        {
+            if (!_currentFsms.ContainsKey(typeof(T)))
+            {
+                var newFsm = _diContainer.Instantiate(typeof(T)) as T;
+                newFsm.Init();
+                _currentFsms.Add(typeof(T), newFsm);
+            }
+            
+            _activeFsm?.Reset();
+            _activeFsm = _currentFsms[typeof(T)];
+            _activeFsm.SetStartState();
+        }
+
+        public Fsm TryGetFsm<T>() where T : Fsm
+        {
+            AddNewFsm<Fsm>();
+            return _currentFsms.TryGetValue(typeof(T), out var fsm) ? fsm : null;
+        }
+
+        public void AddNewFsm<T>() where T : Fsm, new()
+        {
+            if (_currentFsms.ContainsKey(typeof(T)))
+                return;
+
+            var newFsm = new T();
+            newFsm.Init();
+            _currentFsms.Add(typeof(T), newFsm);
+        }
+
+        public void AddNewFsm(IFsm newFsm)
+        {
+            if (_currentFsms.ContainsKey(newFsm.GetType()))
+                return;
+
+            newFsm.Init();
+            _currentFsms.Add(newFsm.GetType(), (Fsm)newFsm);
+        }
+
+        public void RemoveFsm<T>() where T : Fsm
+        {
+            _currentFsms.Remove(typeof(T));
+        }
+
+        public void RemoveFsm(IFsm fsm)
+        {
+            _currentFsms.Remove(fsm.GetType());
+        }
+    }
+}
